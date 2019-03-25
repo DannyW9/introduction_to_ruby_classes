47e470c69ba9d354db4ecaa1e95581a93b907146
Danny Welsh <dannywelsh400@hotmail.com>
2019-03-25 21:22:05 +0000

    adds library class with tests passing (arrays and classes)

--- library_class.rb
+++ library_class.rb
@@ -1,12 +1,41 @@
 class Library
 
-  attr_accessor :book
+  attr_accessor :books
 
   def initialize(books)
     @books = books
   end
 
+  def return_by_name(title)
+    for book in @books
+      if book[:title] == title
+        return book
+      end
+    end
+  end
 
+  def return_rental_details(title)
+    for book in @books
+      if book[:title] == title
+        return book[:rental_details]
+      end
+    end
+  end
+
+  def add_new_book(title)
+    @books.push({title: title, rental_details: {student_name: "", date: ""}})
+    @books.length()
+  end
+
+  def change_rental_details(title, new_name, new_date)
+    for book in @books
+      if book[:title] == title
+        book[:rental_details][:student_name] = new_name
+        book[:rental_details][:date] = new_date
+        return book[:rental_details]
+      end
+    end
+  end
 
 
   end
--- specs/library_classes_spec.rb
+++ specs/library_classes_spec.rb
@@ -5,14 +5,32 @@ require_relative('../library_class.rb')
 class ClassesTest < MiniTest::Test
 
   def setup
-    @book1 = {
-      title: "lord of the rings",
-      rental_details: {
-        student_name: "Jeff",
-        date: "29/03/19"
-      }
-    }
+    @library = Library.new([
+      {title: "lord of the rings", rental_details: {student_name: "Jeff", date: "29/03/19"}},
+      # {title: "of mice and men", rental_details: {student_name: "", date: ""}}
+      ])
   end
+
+
+def test_return_by_name
+  found_book = @library.return_by_name("lord of the rings")
+  assert_equal("lord of the rings", found_book[:title])
+end
+
+def test_return_rental_details
+  found_rental = @library.return_rental_details("lord of the rings")
+  assert_equal({student_name: "Jeff", date: "29/03/19"}, found_rental)
+end
+
+def test_add_new_book
+  book_list = @library.add_new_book("of mice and men")
+  assert_equal(2, book_list)
+end
+
+def test_change_rental_details
+  new_loanee = @library.change_rental_details("lord of the rings", "David", "20/06/19")
+assert_equal({student_name: "David", date: "20/06/19"}, new_loanee)
+end
 
 
 end
