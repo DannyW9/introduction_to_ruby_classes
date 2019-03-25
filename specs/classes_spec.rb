95cc12c2793990ad425244595ebb0130961a131e
Danny Welsh <dannywelsh400@hotmail.com>
2019-03-25 15:30:34 +0000

    adds student class passing all tests (getters and setters)

---
+++ specs/classes_spec.rb
@@ -0,0 +1,40 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../student_class.rb')
+require_relative('../sports_class.rb')
+require_relative('../library_class.rb')
+
+class BankAccountTest < MiniTest::Test
+
+  def setup
+    @student1 = Student.new("John", "G20")
+  end
+
+  def test_get_student_name
+    assert_equal("John", @student1.get_student_name)
+  end
+
+  def test_get_student_cohort
+    assert_equal("G20", @student1.get_student_cohort)
+  end
+
+  def test_set_student_name
+    @student1.set_student_name("James")
+    assert_equal("James", @student1.get_student_name)
+  end
+
+  def test_set_student_cohort
+    @student1.set_student_cohort("G15")
+    assert_equal("G15", @student1.get_student_cohort)
+  end
+
+  def test_student_speech
+    assert_equal("I can talk!", @student1.student_speech)
+  end
+
+  def test_favourite_language
+    @student1.favourite_language("Ruby")
+    assert_equal("I love Ruby!", @student1.favourite_language("Ruby"))
+  end
+
+end
---
+++ student_class.rb
@@ -0,0 +1,36 @@
+class Student
+
+  def initialize(student_name, cohort)
+  @student_name = student_name
+  @cohort = cohort
+  end
+
+  def get_student_name
+    return @student_name
+  end
+
+  def get_student_cohort
+    return @cohort
+  end
+
+  def set_student_name(name)
+    @student_name = name
+  end
+
+  def set_student_cohort(cohort)
+    @cohort = cohort
+  end
+
+  def student_speech
+    return "I can talk!"
+  end
+
+  def favourite_language(language)
+    return "I love " + language + "!"
+  end
+
+
+
+
+
+end
